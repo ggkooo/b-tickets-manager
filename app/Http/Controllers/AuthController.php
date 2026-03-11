8dc5c8b7aa617a531d5676110a4ebbc4003d18d2
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-03-11 09:43:30 -0300

    feat(auth): implement registration and login authentication logic

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     *
+     * @param  \App\Http\Requests\RegisterRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+
+        $user = User::create([
+            'uuid' => Str::uuid(),
+            'name' => $request->name,
+            'login' => $request->login,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User registered successfully',
+            'data' => [
+                'user' => $user
+            ]
+        ], 201);
+    }
+
+    /**
+     * Authenticate a user and return a token.
+     *
+     * @param  \App\Http\Requests\LoginRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only('login', 'password'))) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = User::where('login', $request->login)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Login successful',
+            'data' => [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]
+        ]);
+    }
+}
