ab8e0c67bfd2be1b432a9bf4c3b3a46cca57cb65
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-04-15 14:03:01 -0300

    test(app): update example smoke test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,10 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        config(['app.api_key' => 'test-api-key']);
+
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(401);
     }
 }
