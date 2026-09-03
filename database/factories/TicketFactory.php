b4be2a33db685fc92c135c60e1a95e78bf4d2078
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-09-03 14:03:14 -0300

    chore(cleanup): strip comments from TicketFactory

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -5,16 +5,8 @@ namespace Database\Factories;
 use App\Models\Ticket;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends Factory<Ticket>
- */
 class TicketFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         static $counter = 1;
