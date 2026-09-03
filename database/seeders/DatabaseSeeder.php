0e2e3924e1de70df222b890ab96145be92b869ff
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-09-03 14:03:15 -0300

    chore(cleanup): strip comments from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,6 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         foreach (User::allowedLocations() as $location) {
