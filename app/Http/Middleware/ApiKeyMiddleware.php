0d0b2b38f00b3bd6de832c689d4d71bd98601b1a
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-09-03 13:52:10 -0300

    chore(cleanup): strip comments from ApiKeyMiddleware

--- app/Http/Middleware/ApiKeyMiddleware.php
+++ app/Http/Middleware/ApiKeyMiddleware.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ApiKeyMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         $apiKey = $request->header('X-API-KEY');
